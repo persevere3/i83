137e4f449f7a0f692ef572888474881890ed40ee
Jacob0512 <psves0512@gmail.com>
2023-11-22 11:30:07 +0800

    test

--- stores/meals.js
+++ stores/meals.js
@@ -44,13 +44,13 @@ export const useMealsStore = defineStore('meals', () => {
     },
     'meat1': {
       name: '肉類',
-      optionList: ['豬', '雞', '羊', '魚'],
+      optionList: ['豬', '雞', '羊', '鱈魚'],
       number: 1,
       required: true
     },
     'meat2': {
       name: '肉類',
-      optionList: ['沙朗牛', '豬', '雞', '羊', '魚'],
+      optionList: ['沙朗牛', '豬', '雞', '羊', '鱈魚'],
       number: 2,
       required: true
     },
@@ -63,6 +63,7 @@ export const useMealsStore = defineStore('meals', () => {
   })
 
   const meals = reactive([
+    // 單打獨鬥
     {
       categories: ['單打獨鬥區'],
       popularity: false,
@@ -135,7 +136,7 @@ export const useMealsStore = defineStore('meals', () => {
         }
       ],
       content: [
-        '【沙朗牛/豬/雞/羊/魚】擇二',
+        '【沙朗牛/豬/雞/羊/鱈魚】擇二',
         '醬料擇一',
         '含義大利麵、蛋、蔬菜'
       ],
@@ -282,7 +283,7 @@ export const useMealsStore = defineStore('meals', () => {
     {
       categories: ['單打獨鬥區'],
       popularity: false,
-      name: '腓力牛排8oz',
+      name: '菲力牛排8oz',
       origin: '紐西蘭',
       options: [
         {
@@ -348,7 +349,7 @@ export const useMealsStore = defineStore('meals', () => {
         '醬料擇一',
         '含義大利麵、蛋、蔬菜'
       ],
-      price: 280,
+      price: 260,
     },
     {
       categories: ['單打獨鬥區'],
@@ -362,18 +363,34 @@ export const useMealsStore = defineStore('meals', () => {
       price: 100,
     },
 
+    // 咖哩飯
     {
       categories: ['咖哩飯'],
       popularity: false,
-      name: '起司可樂餅咖哩飯',
+      name: '炸豬排咖哩飯',
       origin: '',
       options: [
 
       ],
       content: [
+        '淋上咖哩醬汁的豬排飯，彼此交織著美味的饗宴。(此描述僅供參考)',
         '含飯、蔬菜、小菜'
       ],
-      price: 260,
+      price: 240,
+    },
+    {
+      categories: ['咖哩飯'],
+      popularity: false,
+      name: '炸雞排咖哩飯',
+      origin: '',
+      options: [
+
+      ],
+      content: [
+        '外酥內嫩的雞排，搭配香氣十足的咖哩醬，美味無比！(此描述僅供參考)',
+        '含飯、蔬菜、小菜'
+      ],
+      price: 240,
     },
     {
       categories: ['咖哩飯'],
@@ -392,30 +409,15 @@ export const useMealsStore = defineStore('meals', () => {
     {
       categories: ['咖哩飯'],
       popularity: false,
-      name: '豬排咖哩飯',
+      name: '起司可樂餅咖哩飯',
       origin: '',
       options: [
 
       ],
       content: [
-        '淋上咖哩醬汁的豬排飯，彼此交織著美味的饗宴。(此描述僅供參考)',
         '含飯、蔬菜、小菜'
       ],
-      price: 240,
-    },
-    {
-      categories: ['咖哩飯'],
-      popularity: false,
-      name: '雞排咖哩飯',
-      origin: '',
-      options: [
-
-      ],
-      content: [
-        '外酥內嫩的雞排，搭配香氣十足的咖哩醬，美味無比！(此描述僅供參考)',
-        '含飯、蔬菜、小菜'
-      ],
-      price: 250,
+      price: 260,
     },
     {
       categories: ['咖哩飯'],
@@ -431,6 +433,7 @@ export const useMealsStore = defineStore('meals', () => {
       price: 260,
     },
 
+    // 分進合擊
     {
       categories: ['分進合擊區'],
       popularity: false,
@@ -455,7 +458,7 @@ export const useMealsStore = defineStore('meals', () => {
         }
       ],
       content: [
-        '厚切沙朗、【豬/雞/羊/魚】擇一',
+        '厚切沙朗、【豬/雞/羊/鱈魚】擇一',
         '醬料擇一',
         '含義大利麵、蛋、蔬菜'
       ],
@@ -485,11 +488,11 @@ export const useMealsStore = defineStore('meals', () => {
         }
       ],
       content: [
-        '帶骨牛小排(4盎司，1片，建議全熟)、【豬/雞/羊/魚】擇一',
+        '帶骨牛小排(4盎司，1片，建議全熟)、【豬/雞/羊/鱈魚】擇一',
         '醬料擇一',
         '含義大利麵、蛋、蔬菜'
       ],
-      price: 340,
+      price: 420,
     },
     {
       categories: ['分進合擊區'],
@@ -506,6 +509,7 @@ export const useMealsStore = defineStore('meals', () => {
       price: 500,
     },
 
+    // 米食
     {
       categories: ['米食區'],
       popularity: false,
@@ -572,6 +576,20 @@ export const useMealsStore = defineStore('meals', () => {
       price: 220,
     },
 
+    // 游擊
+    {
+      categories: ['游擊區'],
+      popularity: false,
+      name: '酥炸脆薯',
+      origin: '',
+      options: [
+
+      ],
+      content: [
+        '外皮黃金酥脆、內裡紮實綿密，令人無法抗拒的美妙小點。(此描述僅供參考)'
+      ],
+      price: 90,
+    },
     {
       categories: ['游擊區'],
       popularity: false,
@@ -582,6 +600,19 @@ export const useMealsStore = defineStore('meals', () => {
       ],
       content: [
         '口感柔軟，肉質細嫩，每一口都能感受到食材的細膩。(此描述僅供參考)'
+      ],
+      price: 90,
+    },
+    {
+      categories: ['游擊區'],
+      popularity: false,
+      name: '洋蔥圈',
+      origin: '',
+      options: [
+
+      ],
+      content: [
+        '外皮酥脆、內裡洋蔥的香甜多汁，絕對滿足您的味蕾！(此描述僅供參考)'
       ],
       price: 90,
     },
@@ -608,32 +639,6 @@ export const useMealsStore = defineStore('meals', () => {
       ],
       content: [
         '魷魚的嚼勁十足，搭配著微鹹的風味，十分開胃。(此描述僅供參考)'
-      ],
-      price: 90,
-    },
-    {
-      categories: ['游擊區'],
-      popularity: false,
-      name: '酥炸脆薯',
-      origin: '',
-      options: [
-
-      ],
-      content: [
-        '外皮黃金酥脆、內裡紮實綿密，令人無法抗拒的美妙小點。(此描述僅供參考)'
-      ],
-      price: 90,
-    },
-    {
-      categories: ['游擊區'],
-      popularity: false,
-      name: '洋蔥圈',
-      origin: '',
-      options: [
-
-      ],
-      content: [
-        '外皮酥脆、內裡洋蔥的香甜多汁，絕對滿足您的味蕾！(此描述僅供參考)'
       ],
       price: 90,
     },
