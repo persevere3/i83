01d6951962ec96c8f3240b13a24dc21bed671c38
Jacob0512 <psves0512@gmail.com>
2024-01-19 16:54:06 +0800

    withCredentials

--- utils/https.js
+++ utils/https.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-let withCredentials = true
-
 const baseURL = process.env.NODE_ENV === "development" ? "/api" : "https://preview.uniqcarttest.com/api"
 
 export const jsonRequest = axios.create({
   baseURL,
   headers: { 'Content-Type': 'application/json' },
-  withCredentials
 })
